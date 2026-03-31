1ff08063525658e0c7257a0293e75d4476445df0
Guilherme Henrique Caetano <guilhermehc.ti@gmail.com>
2026-03-31 11:42:35 -0300

    estudos

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+
+/*
+JEITO 1
+
+var c = 1
+while (c < = 5 ) {
+    console.log (c)
+    c++
+}
+ jeito 2
+
+for (var c = 1 ; c<= 100; c++) {
+    console.log (c)
+}
+*/
+
+console.log ('vai começar...')
+for (var c = 1 ; c<= 4; c++) {
+    console.log (c)
+}
+console.log('FIM')
