84120aebe1e76497235509dbf50d05c46a43d981
Guilherme Henrique Caetano <guilhermehc.ti@gmail.com>
2026-04-01 12:20:36 -0300

    estudo

---
+++ aula012ex/ex015/script.js
@@ -0,0 +1,41 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById ('txtano')
+    var res = document.getElementById ('res')
+    if(fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] tente novamente')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if(fsex[0].checked) {
+            genero = 'Homem'
+            if (idade > 0 && idade < 10){
+                //criança
+            } else if (idade <21){
+                //joven
+            } else if (idade <50){
+                //adulto
+            } else {
+                //idoso
+            }
+        }else if (fsex[1].checked) {
+            genero = 'Mulher'
+             if (idade > 0 && idade < 10){
+                //criança
+            } else if (idade <21){
+                //joven
+            } else if (idade <50){
+                //adulto
+            } else {
+                //idoso
+            }
+        }  
+        res.style.textAlign = 'center'
+        res.innerHTML =`Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
